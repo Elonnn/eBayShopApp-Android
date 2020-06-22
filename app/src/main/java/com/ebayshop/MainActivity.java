be584d898fc0ffe04f9a13d1dc6094f4ab67eb17
Elonnn <yilangxu@usc.edu>
2020-06-22 00:05:33 -0700

    added splash

--- app/src/main/java/com/ebayshop/MainActivity.java
+++ app/src/main/java/com/ebayshop/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        setTheme(R.style.AppTheme);
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
