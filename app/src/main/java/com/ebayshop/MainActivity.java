837777d1aa1cf3f0087861fa43a9ccbc52cff115
Elonnn <yilangxu@usc.edu>
2020-06-21 23:34:40 -0700

    first commit

---
+++ app/src/main/java/com/ebayshop/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ebayshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View view) {
+        EditText keywordsTextField = (EditText) findViewById(R.id.keywordsTextField);
+        String s = keywordsTextField.getText().toString();
+        Log.i("info", " aaaaa");
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(s);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
