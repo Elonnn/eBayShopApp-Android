2585ca5ad765d2f99d4a3b6280ea95af2473af90
Elonnn <yilangxu@usc.edu>
2020-06-23 23:26:01 -0700

    completed dropdown box

--- app/src/main/java/com/ebayshop/MainActivity.java
+++ app/src/main/java/com/ebayshop/MainActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener  {
 
 //    public void onClick(View view) {
 //        EditText keywordsTextField = (EditText) findViewById(R.id.keywordsTextField);
@@ -19,11 +29,47 @@ public class MainActivity extends AppCompatActivity {
 ////        TextView textView = (TextView) findViewById(R.id.textView);
 //        textView.setText(s);
 //    }
+    JSONObject searchFilter = new JSONObject();
+    private static final Map<String, String> optionMap = new HashMap<>();
+    static {
+        optionMap.put("Best Match", "BestMatch");
+        optionMap.put("Price: Highest First", "CurrentPriceHighest");
+        optionMap.put("Price + Shipping: Highest First", "PricePlusShippingHighest");
+        optionMap.put("Price + Shipping: Lowest First", "PricePlusShippingLowest");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sort_by_options, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        try {
+            Log.i("Tapped", String.valueOf(parent.getItemAtPosition(pos)));
+            searchFilter.put("sort_order", optionMap.get((String)parent.getItemAtPosition(pos)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
